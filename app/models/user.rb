47bf624246eb6ff380669fd10eec2e604470a3dc
Juan Roa <jdroa92@gmail.com>
2017-02-13 08:44:43 -0500

    Adding has_many posts to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+class User < ActiveRecord::Base
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts
 end
