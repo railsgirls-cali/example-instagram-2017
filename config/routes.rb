ccd3510a2f5cee6f1fa777e9290a6b4da8094fa6
Juan Roa <jdroa92@gmail.com>
2017-02-13 00:28:10 -0500

    Updating header and posts views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
-  root "pages#home"
+  root "posts#index"
   get "about" => "pages#about"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
